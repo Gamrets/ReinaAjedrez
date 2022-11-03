e7f0f1bae9efc73d5e6d29dc741748626a800d08
Konstantin Krishtapov <constantinopladwar@gmail.com>
2022-11-03 18:56:53 +0100

    4.2 Creo metodos getters y setters

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -2,7 +2,40 @@ package org.iesalandalus.programacion.reinaajedrez.modelo;
 
 public class Posicion {
 
+	// Declaro los atributos
+	
 	private int fila;
 	private char columna;
+	
+	//Creo metodos setter y getter
+
+		private void setFila(int fila) {
+
+			if (fila < 1 || fila > 8) {
+				throw new IllegalArgumentException("ERROR: Fila no válida.");
+
+			} else {
+				this.fila = fila;
+			}
+		}
+
+		public int getFila() {
+			return this.fila;
+		}
+
+		private void setColumna(char columna) {
+
+			if (columna < 'a' || columna > 'h') {
+				throw new IllegalArgumentException("ERROR: Columna no válida.");
+
+			} else {
+				this.columna = columna;
+
+			}
+		}
+
+		public int getColumna() {
+			return this.columna;
+		}
 	
 }
