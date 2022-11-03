c3ff1e2496b930667cd4dbb9a6a3c0beefdca480
Konstantin Krishtapov <constantinopladwar@gmail.com>
2022-11-03 19:25:31 +0100

    5.1 He creado metodos Getter y Setters

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -2,9 +2,36 @@ package org.iesalandalus.programacion.reinaajedrez.modelo;
 
 public class Reina {
 
+	// Declaro atributos
+
 	private Color color;
 	private Posicion posicion;
+
+	// Creo metodos Getters y Setters
+
+	private void setPosicion(Posicion posicion) {
+
+		this.posicion = posicion;
+
+	}
+
+	public Posicion getPosicion() {
+
+		return this.posicion;
+	}
+
+	private void setColor(Color color) {
+
+		if (color == null) {
+			throw new IllegalArgumentException("ERROR: No se puede asignar un color nulo.");
+		}
+		this.color = color;
+	}
+
+	public Color getColor() {
+		return this.color;
+	}
 	
 	
-	
+		
 }
