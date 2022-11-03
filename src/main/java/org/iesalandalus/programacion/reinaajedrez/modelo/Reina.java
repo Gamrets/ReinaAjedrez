9c071de25ea1c9605ba2b3d1486c252bb22d3dc5
Konstantin Krishtapov <constantinopladwar@gmail.com>
2022-11-03 20:52:45 +0100

    5.4.1 He corregido constructor que recibe color y creado metodo Mover (sin acabar).

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -1,15 +1,18 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Reina {
 
 	// Declaro atributos
 
 	private Color color;
 	private Posicion posicion;
-	
-	//Constructor por defecto
-	
+
+	// Constructor por defecto
+
 	public Reina() {
+
 		color = Color.BLANCO;
 		posicion = new Posicion(1, 'd');
 	}
@@ -21,21 +24,24 @@ public class Reina {
 		this.posicion = posicion;
 
 	}
-	
-	//Constructor que acepta parametros color
 
-		public Reina(Color color) {
+	// Constructor que acepta parametros color
 
-			if (color == Color.BLANCO) {
-				posicion = new Posicion(1, 'd');
-			} else if (color == Color.NEGRO) {
-				posicion = new Posicion(8, 'b');
-			}
+	public Reina(Color color) {
 
-			if (color == null) {
-				throw new NullPointerException("ERROR: No se puede asignar un color nulo.");
-			}
+		if (color == null) {
+			throw new NullPointerException("ERROR: El color no puede ser nulo.");
 		}
+
+		setColor(color);
+
+		if (color == color.BLANCO) {
+			posicion = new Posicion(1, 'd');
+		} else if (color == Color.NEGRO) {
+			posicion = new Posicion(8, 'd');
+		}
+
+	}
 
 	public Posicion getPosicion() {
 
@@ -53,7 +59,111 @@ public class Reina {
 	public Color getColor() {
 		return this.color;
 	}
-	
-	
+
+	public void mover(Direccion direccion, int pasos) throws OperationNotSupportedException {
+
+		if (pasos < 1 || pasos > 7) {
+
+			throw new IllegalArgumentException("El número de pasos debe estar comprendido entre 1 y 7.");
+
+		}
+
+		if (direccion == null) {
+
+			throw new NullPointerException("ERROR: La dirección no puede ser nula.");
+		}
+
+		
+		int fila = 0;
+		char columna;
+		
+		try {
+
+			switch (direccion) {
+
+			// Arriba
+			case NORTE: {
+				if (color == Color.BLANCO) {
+					setPosicion(new Posicion(posicion.getFila() + pasos, posicion.getColumna()));
+				} else {
+					setPosicion(new Posicion(posicion.getFila() - pasos, posicion.getColumna()));
+				}
+				break;
+			}
+			// Abajo
+			case SUR: {
+				if (color == Color.BLANCO) {
+					setPosicion(new Posicion(posicion.getFila() - pasos, posicion.getColumna()));
+				} else {
+					setPosicion(new Posicion(posicion.getFila() + pasos, posicion.getColumna()));
+				}
+				break;
+			}
+			// Izquierda
+			case OESTE: {
+				if (color == Color.BLANCO) {
+					setPosicion(new Posicion(posicion.getFila(), (char) (posicion.getColumna() - pasos)));
+				} else {
+					setPosicion(new Posicion(posicion.getFila(), (char) (posicion.getColumna() + pasos)));
+				}
+				break;
+			}
+			// Derecha
+			case ESTE: {
+				if (color == Color.BLANCO) {
+					setPosicion(new Posicion(posicion.getFila(), (char) (posicion.getColumna() + pasos)));
+				} else {
+					setPosicion(new Posicion(posicion.getFila(), (char) (posicion.getColumna() - pasos)));
+				}
+				break;
+			}
+
+			// Arriba-Derecha
+			case NORESTE: {
+
+				/*fila = posicion.getFila() + pasos;
+				columna = (char) ((int) posicion.getColumna() + pasos);
+				setPosicion(new Posicion(fila, columna));
+				*/
+				break;
+			}
+			// Arriba-Izquierda
+			case NOROESTE: {
+
+				/*fila = posicion.getFila() + pasos;
+
+				columna = (char) ((int) posicion.getColumna() - pasos);
+				setPosicion(new Posicion(fila, columna));
+				*/
+				break;
+			}
+
+			// Abajo-Derecha
+			case SURESTE: {
+				
+				/*fila = posicion.getFila() - pasos;
+
+				columna = (char) ((int) posicion.getColumna() + pasos);
+				setPosicion(new Posicion(fila, columna));
+*/
+				break;
+			}
+			// Abajo-Izquierda
+			case SUROESTE: {
+				
+				/*fila = posicion.getFila() - pasos;
+
+				columna = (char) ((int) posicion.getColumna() - pasos);
+				setPosicion(new Posicion(fila, columna));
+*/
+				break;
+			}
+			default:
+			}
+		} catch (IllegalArgumentException e) {
+			throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+		}
+
+	}
 		
 }
