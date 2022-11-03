a7e5c50beaff251cc8bc4bcb77f6b0a84a222ce9
Konstantin Krishtapov <constantinopladwar@gmail.com>
2022-11-03 19:15:29 +0100

    4.6 He creado metodo toString

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -5,82 +5,82 @@ import java.util.Objects;
 public class Posicion {
 
 	// Declaro los atributos
-	
+
 	private int fila;
 	private char columna;
-	
-	
-	//Constructor
- 	public Posicion(int fila,char columna) {
- 		
- 		setFila(fila);
- 		setColumna(columna);
- 	}
- 	
- 	//Creo constructor copia
 
-    public Posicion(Posicion posicion){
+	// Constructor
+	public Posicion(int fila, char columna) {
 
-   	 if (posicion == null) {
+		setFila(fila);
+		setColumna(columna);
+	}
+
+	// Creo constructor copia
+
+	public Posicion(Posicion posicion) {
+
+		if (posicion == null) {
 			throw new NullPointerException("ERROR: No es posible copiar una posición nula.");
 		}
 		this.fila = posicion.getFila();
 		this.columna = posicion.getColumna();
 	}
-	
-	
-	//Creo metodos setter y getter
 
-		private void setFila(int fila) {
+	// Creo metodos setter y getter
 
-			if (fila < 1 || fila > 8) {
-				throw new IllegalArgumentException("ERROR: Fila no válida.");
+	private void setFila(int fila) {
 
-			} else {
-				this.fila = fila;
-			}
+		if (fila < 1 || fila > 8) {
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
+
+		} else {
+			this.fila = fila;
 		}
+	}
 
-		public int getFila() {
-			return this.fila;
+	public int getFila() {
+		return this.fila;
+	}
+
+	private void setColumna(char columna) {
+
+		if (columna < 'a' || columna > 'h') {
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
+
+		} else {
+			this.columna = columna;
+
 		}
+	}
 
-		private void setColumna(char columna) {
+	public char getColumna() {
+		return this.columna;
+	}
 
-			if (columna < 'a' || columna > 'h') {
-				throw new IllegalArgumentException("ERROR: Columna no válida.");
+	// Metodos hashCode y equals
 
-			} else {
-				this.columna = columna;
+	@Override
+	public int hashCode() {
+		return Objects.hash(columna, fila);
+	}
 
-			}
-		}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Posicion other = (Posicion) obj;
+		return columna == other.columna && fila == other.fila;
+	}
 
-		public char getColumna() {
-			return this.columna;
-		}
+	// Creo metodo toString
 
-		
-		//Metodos hashCode y equals
-		
-		@Override
-		public int hashCode() {
-			return Objects.hash(columna, fila);
-		}
+	public String toString() {
+		return "fila=" + fila + ", columna=" + columna;
+	}
 
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Posicion other = (Posicion) obj;
-			return columna == other.columna && fila == other.fila;
-		}
-		
-		
-		
-	
 }
