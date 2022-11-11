6e56d3aa07eb85d27bbf3720dbee67dd55252d58
Konstantin <constantinopladwar@gmail.com>
2022-11-11 13:46:01 +0100

    7.1 He creado atributo creina

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -1,6 +1,12 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import org.iesalandalus.programacion.reinaajedrez.modelo.Reina;
+
 public class MainApp {
+	
+	private static Reina reina;
+	private static Consola consola;
+	private static boolean salir = false;
 	
 	public static void main(String[] args) {
 		
